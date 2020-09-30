2e5bb97d37612f2128919ac30adf3e30abdbb24f
naruthsithasom <ett0088@gmail.com>
2020-09-30 21:27:30 +0700

    Button Add

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { List, Input, Button, Row, Col, Divider, Typography } from 'antd';
 import _ from 'lodash'
 
