8c7b6b80bcc6d6b2f1cb14c8bde57ad5fc7775a5
naruthsithasom <ett0088@gmail.com>
2020-09-30 19:17:17 +0700

    react-js-fullstack

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function TodoList(){
+  return(
+    <div>Todo list page</div>  )
+}
---
+++ src/components/pages/Index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import TodoList from '../TodoList/TodoList'
+
+export default function Index(){
+  return(
+    <div>Todo list page</div>  )
+}
